b3fdf14b8fe41698ecded1e1e620f582ec517ba5
codenutszone <43089700+codenutszone@users.noreply.github.com>
2019-07-18 22:25:03 +0530

    reverse_integer
    Reverse digits of an 32-bit signed integer.

---
+++ reverse_integer.py
@@ -0,0 +1,19 @@
+class Solution():
+    def reverse(self,Number):
+            
+                flag=False
+                if Number < 0:
+                     flag = True
+                     Number=-1*Number
+
+                Reverse = 0
+                while (Number > 0):
+                    Reminder = Number % 10
+                    Reverse = (Reverse * 10) + Reminder
+                    Number = Number //10
+
+                num= -Reverse if (flag == True) else Reverse
+                return num if num in range(-2**31,2**31-1) else 0
+num=int(input("enter the integer number: "))
+s=Solution()
+print(s.reverse(num))
